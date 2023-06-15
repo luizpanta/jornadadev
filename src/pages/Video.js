c4aae9e7b99100a6ab995c922a83b882012f7dc1
luizpanta <luizpantarotti@gmail.com>
2023-06-15 15:52:28 -0300

    Aula 2 video element

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,18 +1,32 @@
-import React from 'react'
-import "./video.css"
+import React, { useRef, useState } from "react";
+import "./video.css";
 
 function Video() {
-  return (
-    <div className='video'>
-      Eu sou um video
-      <video 
-        className='video__player'
-        src="https://www.tiktok.com/@paulosantos2058/video/6809238950750866693"
-      >
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
 
-      </video>
+  function handleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      Eu sou um video
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handleStart}
+        loop
+        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"
+      ></video>
     </div>
-  )
+  );
 }
 
-export default Video
+export default Video;
